fc17b8964959d9ec020d00badcd8216fbd0060e2
Tery Lim <kumabotz@gmail.com>
2022-05-29 19:25:00 +1200

    Initial commit

---
+++ Character.gd
@@ -0,0 +1,35 @@
+extends KinematicBody
+
+export var gravity = Vector3.DOWN * 10
+export var speed = 4
+export var rot_speed = 0.85
+
+var velocity = Vector3.ZERO
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _physics_process(delta):
+	velocity += gravity * delta
+	get_input()
+	velocity = move_and_slide(velocity, Vector3.UP)
+	
+func get_input():
+	var vy = velocity.y
+	velocity = Vector3.ZERO
+	if Input.is_action_pressed("forward"):
+		velocity += -transform.basis.z * speed
+	if Input.is_action_pressed("back"):
+		velocity += transform.basis.z * speed
+	if Input.is_action_pressed("right"):
+		velocity += transform.basis.x * speed
+	if Input.is_action_pressed("left"):
+		velocity += -transform.basis.x * speed
+		
+	velocity.y = vy
